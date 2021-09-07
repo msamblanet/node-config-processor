37ae5f7daba98ea625eaa4c09f474b41dc0e75bb
Michael Samblanet <msamblanet@users.noreply.github.com>
2021-09-07 09:36:57 -0400

    Initial Development A

--- test/configProcessor.test.ts
+++ test/configProcessor.test.ts
@@ -1,9 +1,6 @@
-import * as dotenv from 'dotenv';
-dotenv.config();
-
 import * as Lib from "../src/index";
 import LibDefault from "../src/index";
 
 test("example test", () => {
-    expect(LibDefault).toEqual("Example export");
+    //expect(LibDefault).toEqual("Example export");
 })
